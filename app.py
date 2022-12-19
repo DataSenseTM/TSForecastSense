456d794e741300293765e2e47e65951120fbdd5d
Sanjoy Biswas <sanjoy.eee32@gmail.com>
2022-12-19 11:32:04 +0600

    requirements file update

--- app.py
+++ app.py
@@ -20,11 +20,10 @@ You'll be able to import your data from a CSV file, visualize trends and feature
 """
 df = st.file_uploader('Import the time series csv file here. Columns must be labeled ds and y. The input to Prophet is always a dataframe with two columns: ds and y. The ds (datestamp) column should be of a format expected by Pandas, ideally YYYY-MM-DD for a date or YYYY-MM-DD HH:MM:SS for a timestamp. The y column must be numeric, and represents the measurement we wish to forecast.', type='csv')
 
-st.info(
-            f"""
-                👆 Upload a .csv file first. Sample to try: [peyton_manning_wiki_ts.csv](https://raw.githubusercontent.com/zachrenwick/streamlit_forecasting_app/master/example_data/example_wp_log_peyton_manning.csv)
-                """
-        )
+st.info(f"""
+#Upload a .csv file first. Sample to try: [peyton_manning_wiki_ts.csv](https://raw.githubusercontent.com/zachrenwick/streamlit_forecasting_app/master/example_data/example_wp_log_peyton_manning.csv)
+"""
+)
 
 if df is not None:
     data = pd.read_csv(df)
